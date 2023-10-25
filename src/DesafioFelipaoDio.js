8b90b3af9719519b6e12e4701ae96c90c05b46be
spartacos333 <147752167+spartacos333@users.noreply.github.com>
2023-10-25 09:23:11 -0300

    primeiro up
    acho q acabei de codar, mas vou deixar como teste dessa vez.

--- src/DesafioFelipaoDio.js
+++ src/DesafioFelipaoDio.js
@@ -1,1 +1,31 @@
-console.log("eu consegui mlk!!!")
+// Desafio Classificador de nível de Herói
+let nomeDoHeroi= "spartacos333"
+let XP = 0
+let rankDoHeroi
+XP++
+switch(XP){
+    case XP <= 1000 :
+    console.log("O Herói " + nomeDoHeroi + " está no nível " + "a")
+    break;
+    case XP >= 1001 && XP <= 2000 :
+    console.log("O Herói " + nomeDoHeroi + " está no nível " + "b")
+    break;
+    case XP >= 2001 && XP <= 5000 :
+    console.log("O Herói " + nomeDoHeroi + " está no nível " + "c")
+    break;
+    case XP >= 5001 && XP <= 6000 :
+    console.log("O Herói " + nomeDoHeroi + " está no nível " + "d")
+    break;
+    case XP >= 6001 && XP <= 7000 :
+    console.log("O Herói " + nomeDoHeroi + " está no nível " + "f")
+    break;
+    case XP >= 7001 && XP <= 8000 :
+    console.log("O Herói " + nomeDoHeroi + " está no nível " + "g")
+    break;
+    case XP >= 7001 && XP <= 8000 :
+    console.log("O Herói " + nomeDoHeroi + " está no nível " + "h")
+    break;
+    case XP >= 8001 && XP <= 9000:
+        console.log("O Herói " + nomeDoHeroi + " está no nível " + "j");
+   
+}
