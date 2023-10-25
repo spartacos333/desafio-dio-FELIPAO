dfc0395c4da7c76ffad9db48f8bc80a5cc3574f9
spartacos333 <147752167+spartacos333@users.noreply.github.com>
2023-10-25 09:30:44 -0300

    updateFInalParaODesafio
    alterei a variacao de xp correspondente a um determinado rank.

--- src/DesafioFelipaoDio.js
+++ src/DesafioFelipaoDio.js
@@ -1,31 +1,34 @@
-// Desafio Classificador de nível de Herói
-let nomeDoHeroi= "spartacos333"
-let XP = 0
-let rankDoHeroi
-XP++
-switch(XP){
-    case XP <= 1000 :
-    console.log("O Herói " + nomeDoHeroi + " está no nível " + "a")
-    break;
-    case XP >= 1001 && XP <= 2000 :
-    console.log("O Herói " + nomeDoHeroi + " está no nível " + "b")
-    break;
-    case XP >= 2001 && XP <= 5000 :
-    console.log("O Herói " + nomeDoHeroi + " está no nível " + "c")
-    break;
-    case XP >= 5001 && XP <= 6000 :
-    console.log("O Herói " + nomeDoHeroi + " está no nível " + "d")
-    break;
-    case XP >= 6001 && XP <= 7000 :
-    console.log("O Herói " + nomeDoHeroi + " está no nível " + "f")
-    break;
-    case XP >= 7001 && XP <= 8000 :
-    console.log("O Herói " + nomeDoHeroi + " está no nível " + "g")
-    break;
-    case XP >= 7001 && XP <= 8000 :
-    console.log("O Herói " + nomeDoHeroi + " está no nível " + "h")
-    break;
-    case XP >= 8001 && XP <= 9000:
-        console.log("O Herói " + nomeDoHeroi + " está no nível " + "j");
-   
+// Definindo o nome do herói e a variável de rank
+let nomeDoHeroi = "Spartacos333";
+let rankDoHeroi;
+
+// Loop para simular a aquisição de experiência em incrementos de XP
+for (let XP = 0; XP <= 10000; XP = XP + 500) {
+    console.log("Possui " + XP + " XPs");
+    
+    // Verifica o nível do herói com base na quantidade de XP acumulada
+    if (XP <= 1000) {
+        rankDoHeroi = "Ferro";
+    } else if (XP <= 2000) {
+        rankDoHeroi = "Bronze";
+    } else if (XP <= 5000) {
+        rankDoHeroi = "Prata";
+    } else if (XP <= 6000) {
+        rankDoHeroi = "Ouro";
+    } else if (XP <= 7000) {
+        rankDoHeroi = "Platina";
+    } else if (XP <= 8000) {
+        rankDoHeroi = "Ascendente";
+    } else if (XP <= 9000) {
+        rankDoHeroi = "Imortal";
+    } else {
+        rankDoHeroi = "Radiante";
+    }
+    
+    // Exibe o nível do herói com base no XP atual
+    console.log("O Herói " + nomeDoHeroi + " está no nível " + rankDoHeroi);
 }
+
+// Verifica se o herói atingiu o nível "Radiante" e tem o nome "Spartacos333"
+console.log("Temos um heroi capaz de desafiar o chefao? ", (rankDoHeroi === "Radiante") && (nomeDoHeroi === "Spartacos333"));
+
